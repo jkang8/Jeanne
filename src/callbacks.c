4a41cb883f084cb2d9c6fed577e65528a4c40511
mikeriepe <mike.riepe@yahoo.com>
2015-01-10 13:03:23 -0800

    messages to cara.rowealex.com

---
+++ src/callbacks.c
@@ -0,0 +1,29 @@
+#include <pebble.h>
+#include <callbacks.h>
+
+void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  // Read first item
+  Tuple *t = dict_read_first(iterator);
+
+  // For all items
+  while(t != NULL) {
+    // Which key was received?
+    switch(t->key) {
+    }
+
+    // Look for next item
+    t = dict_read_next(iterator);
+  }
+}
+
+void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
