03e01c5b884b07fdd58fda54d761aefac4db8ff5
jkang8 <john00kang@gmail.com>
2015-01-09 19:19:04 -0800

    Added main skeleton

---
+++ src/jeanne.c
@@ -0,0 +1,20 @@
+#include <pebble.h> 
+
+Window *window;
+TextLayer *text_layer;
+
+static void init() {
+   window = window_create();
+   text_layer = text_layer_create(GRect(0, 0, 144, 154));
+}
+
+static void deinit() {
+   text_layer_destroy(text_layer); 
+   window_destroy(window);
+}
+
+int main(void) {
+   init();
+   app_event_loop();
+   deinit();
+}
