976f41f6916ed3437705cc2738de0491e85eaf90
John Kang <john00kang@gmail.com>
2015-01-09 19:38:38 -0800

    Commented out window stack

--- src/jeanne.c
+++ src/jeanne.c
@@ -6,11 +6,11 @@ TextLayer *text_layer;
 static void init() {
    s_window = window_create();
 
-   window_set_window_handlers(s_main_window, (WindowHandlers) {
+   /*window_set_window_handlers(s_main_window, (WindowHandlers) {
       .load = main_window_load,
       .unload = main_window_unload
    });
-   window_stack_push(s_main_window, true);
+   window_stack_push(s_main_window, true); */
    text_layer = text_layer_create(GRect(0, 0, 144, 154));
 }
 
