beeda69d743470aacb1fa51632cfca1c3f418c51
unknown <mikeriepe@SJC-4184-MRB.achronix.local>
2015-01-10 23:12:51 -0800

    merge conflict fixed

--- src/alarm.c
+++ src/alarm.c
@@ -5,7 +5,6 @@
 
 #define WAKEUP_REASON 0
 #define PERSIST_KEY_WAKEUP_ID 42
-<<<<<<< HEAD
 //#define number_pills 10
 //#define medication "viagra"
 //#define day TODAY
@@ -38,23 +37,9 @@ static void alarm_window_load(Window *window) {
   // Set the icons
 //  action_bar_layer_set_icon(action_bar, BUTTON_ID_SELECT, &my_icon_previous);
 //  action_bar_layer_set_icon(action_bar, BUTTON_ID_DOWN, &my_icon_next);
-=======
-#define day TODAY
 
-WakeupId s_wakeup_id;
-Window *s_ty_window;
-
-void wakeup_handler(WakeupId id, int32_t reason) {
-  // Set off alarm
-  //aquire variable values;
-  //int time = persist_read_int(PERSIST_KEY_TIME);
-  char drugs[64]; //make more robust?
-  persist_read_string(PERSIST_KEY_DRUG,drugs,64);
->>>>>>> 65df900d90887b92b70677c62ffd1698dc06ec01
-  
   // Create alarm TextLayer
   static char s_buffer[64];
-<<<<<<< HEAD
   snprintf(s_buffer, sizeof(s_buffer), "It is time to take %d %s.", number_pills, drug_buffer);
   s_alarm_layer = text_layer_create(GRect(5, 0, window_bounds.size.w - 5, 64));
   /*text_layer_set_background_color(s_alarm_layer, GColorBlack);
@@ -82,21 +67,6 @@ void wakeup_handler(WakeupId id, int32_t reason) {
   text_layer_set_font(s_snooze_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
   text_layer_set_text(s_snooze_layer, "Snooze  ->");
   layer_add_child(window_layer, text_layer_get_layer(s_snooze_layer));
-=======
-  int number_pills = persist_read_int(PERSIST_KEY_AMOUNT);
-  snprintf(s_buffer, sizeof(s_buffer), "Take %d %s as soon as possible.", number_pills, drugs);
-  text_layer_set_background_color(g_main_layer, GColorWhite);
-  text_layer_set_text_color(g_main_layer, GColorBlack);
-  text_layer_set_text_alignment(g_main_layer, GTextAlignmentLeft);
-  text_layer_set_font(g_main_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
-  text_layer_set_text(g_main_layer, s_buffer);
-  
-  g_main_layer = text_layer_create(GRect(0, 10, 20, 10));
-  text_layer_set_text(g_main_layer, "Taken ->");
-  
-  g_main_layer = text_layer_create(GRect(0, 20, 20, 10));
-  text_layer_set_text(g_main_layer, "Snooze ->");
->>>>>>> 65df900d90887b92b70677c62ffd1698dc06ec01
   
   //vibrates the watch
   vibes_long_pulse();
@@ -136,25 +106,7 @@ void wakeup_handler(WakeupId id, int32_t reason) {
 }
 
 void check_wakeup() {
-<<<<<<< HEAD
   // No longer applicable
-=======
-  // Get the ID
-  s_wakeup_id = persist_read_int(PERSIST_KEY_TIME);
-  
-  if (s_wakeup_id > 0) {
-    // There is a wakeup scheduled soon
-    time_t timestamp = 0;
-    wakeup_query(s_wakeup_id, &timestamp);
-    int number_pills = persist_read_int(PERSIST_KEY_AMOUNT);
-    char drugs[64];
-    persist_read_string(PERSIST_KEY_DRUG, drugs, 64);
-    // Show how many seconds to go
-    static char s_buffer[64];
-    snprintf(s_buffer, sizeof(s_buffer), "Take %d %s!", number_pills, drugs);
-    text_layer_set_text(g_main_layer, s_buffer);
-  }
->>>>>>> 65df900d90887b92b70677c62ffd1698dc06ec01
 }
 
 void select_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -162,7 +114,6 @@ void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   // Delete the ID 
     persist_delete(PERSIST_KEY_WAKEUP_ID);
 	
-<<<<<<< HEAD
 	// Kill alarm layers
 	void action_bar_layer_destroy(ActionBarLayer *action_bar);
 	// Add bitmap smiley face
@@ -173,15 +124,6 @@ void select_click_handler(ClickRecognizerRef recognizer, void *context) {
 	//Wait 3 seconds
 	psleep(300); 
 	//call deinit?
-=======
-	// Open a thank you window
-	s_ty_window = window_create();
-	//add bitmap smiley face
-	g_main_layer = text_layer_create(GRect(0, 10, 20, 10));
-	text_layer_set_font(g_main_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
-	text_layer_set_text(g_main_layer, "Thank you!");
-	//Move back to main window after 5 seconds?
->>>>>>> 65df900d90887b92b70677c62ffd1698dc06ec01
 }
 
 void down_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -204,14 +146,8 @@ void up_click_handler(ClickRecognizerRef recognizer, void *context) {
   s_wakeup_id = wakeup_schedule(future_time, WAKEUP_REASON, true);
   persist_write_int(PERSIST_KEY_WAKEUP_ID, s_wakeup_id);
 
-<<<<<<< HEAD
   // Prepare for waking up later
   //text_layer_set_text(s_output_layer, "An alarm will go off in 2 seconds.");
-=======
-    // Prepare for waking up later
-    text_layer_set_text(g_main_layer, "An alarm will go off in 5 seconds.");
-	}
->>>>>>> 65df900d90887b92b70677c62ffd1698dc06ec01
 }
 
 void click_config_provider(void *context) {
