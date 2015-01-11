de195e2078fbc662ed9f37068eda03b8497d30fd
unknown <mikeriepe@SJC-4184-MRB.achronix.local>
2015-01-10 17:42:24 -0800

    clean up javascript

--- src/PebbleKit.js
+++ src/PebbleKit.js
@@ -7,40 +7,16 @@ var xhrRequest = function (url, type, callback) {
   xhr.send();
 };
 
+// Send patients location to the caregiver upon request
 function locationSuccess(pos) {
   // Construct URL
-  var url = "http://api.openweathermap.org/data/2.5/weather?lat=" +
+  var url = "http://cara.rowealex.com/location?lat=" +
       pos.coords.latitude + "&lon=" + pos.coords.longitude;
 
   // Send request to OpenWeatherMap
   xhrRequest(url, 'GET', 
     function(responseText) {
-      // responseText contains a JSON object with weather info
-      var json = JSON.parse(responseText);
-
-      // Temperature in Kelvin requires adjustment
-      var temperature = Math.round(json.main.temp - 273.15);
-      console.log("Temperature is " + temperature);
-
-      // Conditions
-      var conditions = json.weather[0].main;      
-      console.log("Conditions are " + conditions);
-      
-      // Assemble dictionary using our keys
-      var dictionary = {
-        "KEY_TEMPERATURE": temperature,
-        "KEY_CONDITIONS": conditions
-      };
-
-      // Send to Pebble
-      Pebble.sendAppMessage(dictionary,
-        function(e) {
-          console.log("Weather info sent to Pebble successfully!");
-        },
-        function(e) {
-          console.log("Error sending weather info to Pebble!");
-        }
-      );
+      // no response expected
     }      
   );
 }
@@ -49,7 +25,7 @@ function locationError(err) {
   console.log("Error requesting location!");
 }
 
-function getWeather() {
+function sendLocation() {
   navigator.geolocation.getCurrentPosition(
     locationSuccess,
     locationError,
@@ -75,9 +51,6 @@ function getMedication() {
 Pebble.addEventListener('ready', 
   function(e) {
     console.log("PebbleKit JS ready!");
-
-    // Get the initial weather
-    //getWeather();
     getMedication();
   }
 );
@@ -86,7 +59,6 @@ Pebble.addEventListener('ready',
 Pebble.addEventListener('appmessage',
   function(e) {
     console.log("AppMessage received!");
-    //getWeather();
     getMedication();
   }                     
 );
