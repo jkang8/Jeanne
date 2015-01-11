dcc80a582b2fbf0abda6482d28865b683796b061
aprowe <aprowe@ucsc.edu>
2015-01-11 03:24:56 -0800

    Cleanup

--- src/PebbleKit.js
+++ src/PebbleKit.js
@@ -7,63 +7,6 @@ var xhrRequest = function (url, type, callback) {
   xhr.send();
 };
 
-function send_help() {
-    var url = "http://cara.rowealex.com/create_alert?message=Help+I+Fell+And+My+Dog"
-	xhrRequest(url, 'GET',
-      function(responseText){}
-    );
-}
-
-function locationSuccess(pos) {
-  // Construct URLA
-  var url = "http://api.openweathermap.org/data/2.5/weather?lat=" +
-      pos.coords.latitude + "&lon=" + pos.coords.longitude;
-
-  // Send request to OpenWeatherMap
-  xhrRequest(url, 'GET', 
-    function(responseText) {
-      // responseText contains a JSON object with weather info
-      var json = JSON.parse(responseText);
-
-      // Temperature in Kelvin requires adjustment
-      var temperature = Math.round(json.main.temp - 273.15);
-      console.log("Temperature is " + temperature);
-
-      // Conditions
-      var conditions = json.weather[0].main;      
-      console.log("Conditions are " + conditions);
-      
-      // Assemble dictionary using our keys
-      var dictionary = {
-        "KEY_TEMPERATURE": temperature,
-        "KEY_CONDITIONS": conditions
-      };
-
-      // Send to Pebble
-      Pebble.sendAppMessage(dictionary,
-        function(e) {
-          console.log("Weather info sent to Pebble successfully!");
-        },
-        function(e) {
-          console.log("Error sending weather info to Pebble!");
-        }
-      );
-    }      
-  );
-}
-
-function locationError(err) {
-  console.log("Error requesting location!");
-}
-
-function getWeather() {
-  navigator.geolocation.getCurrentPosition(
-    locationSuccess,
-    locationError,
-    {timeout: 15000, maximumAge: 60000}
-  );
-}
-
 function getMedication() {
   // Construct URL
   var url = "http://cara.rowealex.com/today";
@@ -89,12 +32,11 @@ function getMedication() {
       var time = json.time;      
       console.log("Time are " + time);
 
-      // Assemble dictionary using our keys
+      var message = 'You need to take '+ amount+" of "+drug;
+
       var dictionary = {
-        "KEY_DRUG": drug,
-//         "KEY_TIME": time, 
-//         "KEY_AMOUNT": amount
-		
+        "KEY_MESSAGE": message,
+        "KEY_TIME": 'time',
       };
 
       // Send to Pebble
@@ -110,12 +52,12 @@ function getMedication() {
   );
 }
 
+
 // Listen for when the watchface is opened
 Pebble.addEventListener('ready', 
   function(e) {
     console.log("PebbleKit JS ready!");
 
-    // Get the initial weather
     getMedication();
     var looper = function(){
       getMedication();
@@ -124,17 +66,5 @@ Pebble.addEventListener('ready',
       };
     looper();
     }
-);
 
-// Listen for when an AppMessage is received
-Pebble.addEventListener('appmessage',
-  function(e) {
-    console.log("AppMessage received:" + JSON.stringify(e.payload));
-    if (e.payload[5] == 5){
-       send_help();
-    }else {
-       getMedication();
-    }
-
-  }                     
 );
--- src/main.c
+++ src/main.c
@@ -2,204 +2,120 @@
 
 #define WAKEUP_REASON 0
 #define PERSIST_KEY_WAKEUP_ID 42
-#define PERSIST_KEY_TIME 1
-#define KEY_DRUG 0
+#define KEY_MESSAGE 1
+#define KEY_TIME 2
 
-  
+	
 static Window *s_main_window;
 static TextLayer *s_output_layer;
 
 static WakeupId s_wakeup_id;
 
 static void wakeup_handler(WakeupId id, int32_t reason) {
-  // The app has woken!
-  text_layer_set_text(s_output_layer, "Wakey wakey!");
+	// The app has woken!
+	text_layer_set_text(s_output_layer, persist_read_int(KEY_MESSAGE));
 
-  // Delete the ID
-  persist_delete(PERSIST_KEY_WAKEUP_ID);
-}
-
-static void check_wakeup() {
-//   // Get the ID
-//   s_wakeup_id = persist_read_int(PERSIST_KEY_WAKEUP_ID);
-
-//   if (s_wakeup_id > 0) {
-//     // There is a wakeup scheduled soon
-//     time_t timestamp = 0;
-//     wakeup_query(s_wakeup_id, &timestamp);
-//     int seconds_remaining = timestamp - time(NULL);
-
-//     // Show how many seconds to go
-//     static char s_buffer[64];
-//     snprintf(s_buffer, sizeof(s_buffer), "The event is already scheduled for %d seconds from now!", seconds_remaining);
-//     text_layer_set_text(s_output_layer, s_buffer);
-//   }
+	// Delete the ID
+	persist_delete(PERSIST_KEY_WAKEUP_ID);
 }
 
 
 void inbox_received_callback(DictionaryIterator *iterator, void *context) {
-  // Read first item
-  Tuple *t = dict_read_first(iterator);
-           
-  wakeup_cancel_all();
-          time_t future_time = time(NULL) + 5;
-         WakeupId x_wakeup_id = wakeup_schedule(future_time,1,true);
-         persist_write_int(PERSIST_KEY_WAKEUP_ID, x_wakeup_id);
-         APP_LOG(APP_LOG_LEVEL_DEBUG, "made Wakup");
-  
-  // For all items
-  while(t != NULL) {
-    // Which key was received?
-    switch(t->key) {
-      case 0:
-        persist_write_int(PERSIST_KEY_TIME, t->value->int32);
-        // make_wakeup(persist_read_int(PERSIST_KEY_TIME));
+	// Read first item
+	Tuple *t = dict_read_first(iterator);
+					 
+	
 
-        break;
-      case 1:
-//         persist_write_string(PERSIST_KEY_DRUG,t->value->cstring);
-        break;
-      case 2:
-//         persist_write_int(PERSIST_KEY_AMOUNT,t->value->int32);
-        break;
-    }
+	while(t != NULL) {
+		switch(t->key) {
+			case KEY_MESSAGE:
 
-    // Look for next item
-    t = dict_read_next(iterator);
-  }
+				persist_write_int(KEY_MESSAGE, t->value->cstr);
+
+			break;
+
+			case KEY_TIME:
+
+				wakeup_cancel_all();
+				WakeupId x_wakeup_id = wakeup_schedule(_time,1,true);
+				persist_write_int(PERSIST_KEY_WAKEUP_ID, x_wakeup_id);
+				APP_LOG(APP_LOG_LEVEL_DEBUG, "made Wakup");
+
+			break;
+		}
+		t = dict_read_next(iterator);
+	}
+
 }
 
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-//   //Check the event is not already scheduled
-//   if (!wakeup_query(s_wakeup_id, NULL)) {
-//     // Current time + 30 seconds
-//     time_t future_time = time(NULL) + 30;
 
-//     // Schedule wakeup event and keep the WakeupId
-//     s_wakeup_id = wakeup_schedule(future_time, WAKEUP_REASON, true);
-//     persist_write_int(PERSIST_KEY_WAKEUP_ID, s_wakeup_id);
-
-//     // Prepare for waking up later
-//     text_layer_set_text(s_output_layer, "This app will now wake up in 30 seconds.\n\nClose me!");
-//   } else {
-//     // Check existing wakeup
-//     check_wakeup();
-//   }
 }
 
 
 
 static void click_config_provider(void *context) {
-  // Register the ClickHandlers
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+	window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
 }
 
 static void main_window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect window_bounds = layer_get_bounds(window_layer);
+	Layer *window_layer = window_get_root_layer(window);
+	GRect window_bounds = layer_get_bounds(window_layer);
 
-  // Create output TextLayer
-  s_output_layer = text_layer_create(GRect(0, 0, window_bounds.size.w, window_bounds.size.h));
-  text_layer_set_text_alignment(s_output_layer, GTextAlignmentCenter);
-  text_layer_set_text(s_output_layer, "Press SELECT to schedule a Wakeup.");
-  layer_add_child(window_layer, text_layer_get_layer(s_output_layer));
+	// Create output TextLayer
+	s_output_layer = text_layer_create(GRect(0, 0, window_bounds.size.w, window_bounds.size.h));
+	text_layer_set_text_alignment(s_output_layer, GTextAlignmentCenter);
+	text_layer_set_text(s_output_layer, "Press SELECT to schedule a Wakeup.");
+	layer_add_child(window_layer, text_layer_get_layer(s_output_layer));
 }
 
 static void main_window_unload(Window *window) {
-  // Destroy output TextLayer
-  text_layer_destroy(s_output_layer);
+	// Destroy output TextLayer
+	text_layer_destroy(s_output_layer);
 }
 
 static void init(void) {
-  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
-    app_message_register_inbox_received(inbox_received_callback);
-  // Create main Window
-  s_main_window = window_create();
-  window_set_click_config_provider(s_main_window, click_config_provider);
-  window_set_window_handlers(s_main_window, (WindowHandlers) {
-    .load = main_window_load,
-    .unload = main_window_unload,
-  });
-  window_stack_push(s_main_window, true);
+	app_message_register_inbox_received(inbox_received_callback);
 
-  // Subscribe to Wakeup API
-  wakeup_service_subscribe(wakeup_handler);
+	app_message_open(
+		app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
 
-  // Was this a wakeup launch?
-  if (launch_reason() == APP_LAUNCH_WAKEUP) {
-    // The app was started by a wakeup
-    WakeupId id = 0;
-    int32_t reason = 0;
+	// Create main Window
+	s_main_window = window_create();
+	window_set_click_config_provider(s_main_window, click_config_provider);
+	window_set_window_handlers(s_main_window, (WindowHandlers) {
+		.load = main_window_load,
+		.unload = main_window_unload,
+	});
 
-    // Get details and handle the wakeup
-    wakeup_get_launch_event(&id, &reason);
-    wakeup_handler(id, reason);
-  } else {
-    // Check whether a wakeup will occur soon
-    check_wakeup();
-  }
-  
+	window_stack_push(s_main_window, true);
+
+	// Subscribe to Wakeup API
+	wakeup_service_subscribe(wakeup_handler);
+
+	// Was this a wakeup launch?
+	if (launch_reason() == APP_LAUNCH_WAKEUP) {
+		// The app was started by a wakeup
+		WakeupId id = 0;
+		int32_t reason = 0;
+
+		// Get details and handle the wakeup
+		wakeup_get_launch_event(&id, &reason);
+		wakeup_handler(id, reason);
+	} 
 
 }
 
 static void deinit(void) {
-  // Destroy main Window
-  window_destroy(s_main_window);
+	// Destroy main Window
+	window_destroy(s_main_window);
 }
 
 int main(void) {
-  init();
-  app_event_loop();
-  deinit();
+	init();
+	app_event_loop();
+	deinit();
 }
 
-
-
-
-// void make_wakeup(int time){
-
-//   // Current time + 30 seconds
-//   time_t future_time = time(NULL) + 30;
-
-//   // Schedule wakeup event and keep the WakeupId
-//   s_wakeup_id = wakeup_schedule(future_time, WAKEUP_REASON, true);
-//   persist_write_int(PERSIST_KEY_WAKEUP_ID, s_wakeup_id);
-
-//   // Prepare for waking up later
-//   text_layer_set_text(s_output_layer, "This app will now wake up in 30 seconds.\n\nClose me!");
-
-//    wakeup_cancel_all();
-//    WakeupId x_wakeup_id = wakeup_schedule(time,1,true);
-//    persist_write_int(PERSIST_KEY_WAKEUP_ID, x_wakeup_id);
-
-//    APP_LOG(APP_LOG_LEVEL_DEBUG, "third");
-// }
-
-// void make_wakeup(int time){
-//    wakeup_cancel_all();
-//    WakeupId x_wakeup_id = wakeup_schedule(time,1,true);
-//    persist_write_int(PERSIST_KEY_WAKEUP_ID, x_wakeup_id);
-//    APP_LOG(APP_LOG_LEVEL_DEBUG, "made Wakup");
-// }
-
-
-
-/**************************
-* Callbacks
-****************************/
-
-
-
-void inbox_dropped_callback(AppMessageResult reason, void *context) {
-  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
-}
-
-void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
-  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
-}
-
-void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
-  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
-}
 
