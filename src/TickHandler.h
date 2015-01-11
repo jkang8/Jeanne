d8c1781b7c27b603c972fcc704c282b6ad438fc2
kmcgover <kmcgover@ucsc.edu>
2015-01-10 22:16:23 -0800

    fixed yo

--- src/TickHandler.h
+++ src/TickHandler.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <pebble.h>
-void tick_handler_medication(struct tm *tick_time, TimeUnits units_changed));
+void tick_handler_medication(struct tm *tick_time, TimeUnits units_changed);
 void update_time();
 
 extern TextLayer* s_time_layer;
