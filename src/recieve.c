b860d09954a4fda475eab09c423c3cbd9a024fb3
kmcgover <kmcgover@ucsc.edu>
2015-01-11 00:12:01 -0800

    update alarm

--- src/recieve.c
+++ src/recieve.c
@@ -7,7 +7,7 @@ void receive_key_value(Tuple *t) {
   switch(t->key) {
     case 0:
       persist_write_int(PERSIST_KEY_TIME, t->value->int32);
-	  make_wakeup(t->value->int32);
+	  make_wakeup(persist_read_int(PERSIST_KEY_TIME));
       break;
     case 1:
       persist_write_string(PERSIST_KEY_DRUG,t->value->cstring);
@@ -20,6 +20,6 @@ void receive_key_value(Tuple *t) {
 
 void make_wakeup(int time){
    wakeup_cancel_all();
-   x_wakeup_id = wakeup_schedule(time,1,true);
+   WakeupId x_wakeup_id = wakeup_schedule(time,1,true);
    persist_write_int(PERSIST_KEY_WAKEUP_ID, x_wakeup_id);
 }
