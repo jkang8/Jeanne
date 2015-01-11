b8b46d1a617ad7ed46ceafda6a11a9e1d58a7f9f
unknown <mikeriepe@SJC-4184-MRB.achronix.local>
2015-01-10 19:31:03 -0800

    compile error

--- src/windowHome.h
+++ src/windowHome.h
@@ -4,3 +4,5 @@ extern void window_home_load(Window *window);
 extern void window_home_unload(Window *window);
 extern void window_home_init();
 extern void window_home_deinit();
+
+TextLayer *g_main_layer;
